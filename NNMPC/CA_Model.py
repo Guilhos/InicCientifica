06b1d4da86772322376a552c9e1c69993224aae5
Guilhos <guilhermesfreire1510@gmail.com>
2025-02-05 17:35:38 -0300

    Falta normalizar o CA_model

--- NNMPC/CA_Model.py
+++ NNMPC/CA_Model.py
@@ -7,39 +7,36 @@ import casadi as ca
 class CA_Model:
     def __init__(self, params):
         self.params = params
-        self.lstm_function = self._build_model()
+        self.f_function = self._build_model()
 
-    def sigmoid(x):
+    def sigmoid(self, x):
         return 1 / (1 + ca.exp(-x))
 
-    def lstm_step(self, x_t, h_t, c_t, W, b):
+    def lstm_step(self, x_t, h_t, c_t, Wi, Wh, bi, bh):
         """ Executa uma única etapa da célula LSTM. """
-        concat_input = ca.vertcat(x_t, h_t)
         
         # Gates
-        f_t = self.sigmoid(W['ih'] @ concat_input + W['hh'] @ h_t + b['ih'] + b['hh'])  # Forget gate
-        i_t = self.sigmoid(W['ih'] @ concat_input + W['hh'] @ h_t + b['ih'] + b['hh'])  # Input gate
-        o_t = self.sigmoid(W['ih'] @ concat_input + W['hh'] @ h_t + b['ih'] + b['hh'])  # Output gate
+        i_t = self.sigmoid(Wi[0] @ x_t + Wh[0] @ h_t + bi[0] + bh[0])  # Forget gate
+        f_t = self.sigmoid(Wi[1] @ x_t + Wh[1] @ h_t + bi[1] + bh[1])  # Input gate
+        o_t = self.sigmoid(Wi[3] @ x_t + Wh[3] @ h_t + bi[3] + bh[3])  # Output gate
         
         # Candidato à célula
-        c_hat_t = ca.tanh(W['ih'] @ concat_input + W['hh'] @ h_t + b['ih'] + b['hh'])
+        g_t = ca.tanh(Wi[2] @ x_t + Wh[2] @ h_t + bi[2] + bh[2])
         
         # Atualização da célula e estado oculto
-        c_t_new = f_t * c_t + i_t * c_hat_t
+        c_t_new = f_t * c_t + i_t * g_t
         h_t_new = o_t * ca.tanh(c_t_new)
         
         return h_t_new, c_t_new
 
-    def lstm_layer(self, x_seq, W, b, h0, c0):
+    def lstm_layer(self, x_seq, Wi, Wh, bi, bh, h0, c0):
         """ Executa uma camada LSTM para uma sequência de entrada. """
         h_t, c_t = h0, c0
-        h_seq = []
         
         for x_t in x_seq:
-            h_t, c_t = self.lstm_step(x_t, h_t, c_t, W, b)
-            h_seq.append(h_t)
+            h_t, c_t = self.lstm_step(x_t, h_t, c_t, Wi, Wh, bi, bh)
         
-        return ca.horzcat(*h_seq)  # Concatena ao longo do tempo
+        return ca.horzcat(h_t)  # Concatena ao longo do tempo
 
     def dense_layer(self, x, W, b, activation='tanh'):
         """ Camada densa com ativação """
@@ -48,18 +45,19 @@ class CA_Model:
 
     def _build_model(self):
         x_seq = [ca.MX.sym(f'x_{i}', 4, 1) for i in range(3)]  # Sequência de entrada simbólica
+        2 * (inputs - self.x_min) / (self.x_max - self.x_min) - 1
 
         params = self.params
         
         output = self.dense_layer(
             self.dense_layer(
-                self.lstm_layer(x_seq, params['W_lstm'], params['b_lstm'], np.random.rand(60, 1), np.random.rand(60, 1)),
-                params['W_dense1'], params['b_dense1']
+                self.lstm_layer(x_seq, params[0][0], params[0][1], params[0][2], params[0][3], np.random.rand(60, 1), np.random.rand(60, 1)),
+                params[1][0], params[1][1]
             ),
-            params['W_dense2'], params['b_dense2']
+            params[2][0], params[2][1]
         )
         
-        return ca.Function('LSTM_Model', x_seq + [ca.MX.sym('h0', 60, 1), ca.MX.sym('c0', 60, 1)], [output])
+        return ca.Function('LSTM_Model', x_seq, [output])
 
 
 if __name__ == '__main__':
@@ -74,42 +72,72 @@ if __name__ == '__main__':
     dU = [[0.05],[2000],[-0.02],[-1000],[0.1],[1500]]
     dU = np.concatenate((np.array(dU), np.zeros((int(nU) * (p-m), 1))))
     print(y0.shape, u0.shape)
+    x0 = ca.horzcat(y0,u0)
+    print(x0.shape)
 
     # Carregar os pesos do modelo salvos
     model_path = "NNMPC/libs/modelo_treinado.pth"
     state_dict = torch.load(model_path)
-    print(state_dict.keys())
-
-    # Função para converter arrays do NumPy para matrizes do CasADi (ca.DM)
-    def to_casadi(tensor):
-        return ca.DM(tensor.numpy())
-
+    
+    Wi = state_dict['rnn_layer.weight_ih_l0'][:].numpy()
+    Wh = state_dict['rnn_layer.weight_hh_l0'][:].numpy()
+    Bi = state_dict['rnn_layer.bias_ih_l0'][:].numpy()
+    Bh = state_dict['rnn_layer.bias_hh_l0'][:].numpy()
+    print(Wi.shape, Wh.shape, Bi.shape, Bh.shape)
+    
+    Wii = ca.DM(Wi[:60])
+    Wif = ca.DM(Wi[60:120])
+    Wig = ca.DM(Wi[120:180])
+    Wio = ca.DM(Wi[180:240])
+    Wi = [Wii,Wif,Wig,Wio]
+    
+    Whi = ca.DM(Wh[:60])
+    Whf = ca.DM(Wh[60:120])
+    Whg = ca.DM(Wh[120:180])
+    Who = ca.DM(Wh[180:240])
+    Wh = [Whi, Whf, Whg, Who]
+    
+    Bii = ca.DM(Bi[:60].reshape(-1, 1))
+    Bif = ca.DM(Bi[60:120].reshape(-1, 1))
+    Big = ca.DM(Bi[120:180].reshape(-1, 1))
+    Bio = ca.DM(Bi[180:240].reshape(-1, 1))
+    Bi = [Bii,Bif,Big,Bio]
+    
+    Bhi = ca.DM(Bh[:60].reshape(-1, 1))
+    Bhf = ca.DM(Bh[60:120].reshape(-1, 1))
+    Bhg = ca.DM(Bh[120:180].reshape(-1, 1))
+    Bho = ca.DM(Bh[180:240].reshape(-1, 1))
+    Bh = [Bhi,Bhf,Bhg,Bho]
+    
+    LSTM = [Wi,Wh,Bi,Bh]
+    
+    Wd1 = ca.DM(state_dict['dense_layers.0.weight'].numpy())
+    Bd1 = ca.DM(state_dict['dense_layers.0.bias'].numpy())
+    Dense1 = [Wd1,Bd1]
+    
+    Wd2 = ca.DM(state_dict['dense_layers.2.weight'].numpy())
+    Bd2 = ca.DM(state_dict['dense_layers.2.bias'].numpy())
+    Dense2 = [Wd2,Bd2]
+    
+    #print(Wd1.shape, Bd1.shape, Wd2.shape, Bd2.shape)
+    
     # Organizar os pesos e bias no formato esperado
-    params = {
-        'W_lstm': {
-            'ih': to_casadi(state_dict['rnn_layer.weight_ih_l0']),  # Pesos entrada -> gates
-            'hh': to_casadi(state_dict['rnn_layer.weight_hh_l0'])   # Pesos estado oculto -> gates
-        },
-        'b_lstm': {
-            'ih': to_casadi(state_dict['rnn_layer.bias_ih_l0']),    # Bias entrada -> gates
-            'hh': to_casadi(state_dict['rnn_layer.bias_hh_l0'])     # Bias estado oculto -> gates
-        },
-        'W_dense1': to_casadi(state_dict['dense_layers.0.weight']),  # Pesos da primeira camada densa
-        'b_dense1': to_casadi(state_dict['dense_layers.0.bias']),    # Bias da primeira camada densa
-        'W_dense2': to_casadi(state_dict['dense_layers.2.weight']),  # Pesos da segunda camada densa
-        'b_dense2': to_casadi(state_dict['dense_layers.2.bias'])     # Bias da segunda camada densa
-    }
+    params = [LSTM,Dense1,Dense2]
 
     # Criar a instância da rede CasADi
     Modelo = CA_Model(params)
+    
+    # Extrai cada linha de x0
+    x1 = x0[0, :]  # Primeira linha, shape (1, 4)
+    x2 = x0[1, :]  # Segunda linha, shape (1, 4)
+    x3 = x0[2, :]  # Terceira linha, shape (1, 4)
 
-    # Construir x_seq a partir de y0 e u0
-    x_seq = np.concatenate((y0, u0), axis=1)  # Formato (3, 4)
-    x_seq = np.expand_dims(x_seq, axis=0)  # Formato (1, 3, 4)
+    h0 = ca.DM(np.random.rand(60, 1))  # Estado oculto inicial
+    c0 = ca.DM(np.random.rand(60, 1))
+    
+    print(Modelo.f_function)
 
-    h0 = to_casadi(np.random.rand(60, 1))  # Estado oculto inicial
-    c0 = to_casadi(np.random.rand(60, 1))
-
-    saida =  Modelo.lstm_function(x_seq, h0, c0)
+    saida = Modelo.f_function(x1,x2,x3)
 
     print("Saída da rede CasADi:", saida)
+    print('AAAA')
