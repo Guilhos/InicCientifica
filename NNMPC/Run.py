35819404a69ec481262c1c1bb21e5f597bdb16a8
Guilhos <guilhermesfreire1510@gmail.com>
2025-05-20 20:55:36 -0300

    Commit normais voltaram preciso SINTONIZAR

--- NNMPC/Run.py
+++ NNMPC/Run.py
@@ -3,9 +3,9 @@ from NNMPC import PINN_MPC
 import pickle
 
 qVazao_NN = 1/12.5653085708618164062**2
-qPressao_NN = 0.018995940612126103/9.30146217346191406250**2
-rAlpha_NN = 1.2078697749529814e-05/0.15**2
-rN_NN = 1.6860828534914547e-05/5000**2
+qPressao_NN = 0.05/9.30146217346191406250**2
+rAlpha_NN = 1.2631022545984978e-05/0.15**2
+rN_NN = 0.00019168595642344978/5000**2
 qVazao_CA = 1/12.5653085708618164062**2
 qPressao_CA = 0.05/9.30146217346191406250**2
 rAlpha_CA = 1e-06/0.15**2
@@ -16,10 +16,10 @@ p, m, q_NN, r_NN, q_CA, r_CA, steps = 12, 3, [qVazao_NN,qPressao_NN], [rAlpha_NN
 NNMPC = PINN_MPC(p, m, q_NN, r_NN, steps)
 CAMPC = Only_NMPC(p, m, q_CA, r_CA, steps)
 iter_NN, Ymk_NN, Ypk_NN, Upk_NN, dURot_NN, dUAlpha_NN, YspM_NN, YspP_NN, YmMin_NN, Tempos_NN, PHI_NN = NNMPC.run()
-iter_CA, Ymk_CA, Ypk_CA, Upk_CA, dURot_CA, dUAlpha_CA, YspM_CA, YspP_CA, YmMin_CA, Tempos_CA, PHI_CA = CAMPC.run()
+#iter_CA, Ymk_CA, Ypk_CA, Upk_CA, dURot_CA, dUAlpha_CA, YspM_CA, YspP_CA, YmMin_CA, Tempos_CA, PHI_CA = CAMPC.run()
 
 with open('NNMPC/libs/resultados_NNMPC.pkl', 'wb') as f:
     pickle.dump((iter_NN, Ymk_NN, Ypk_NN, Upk_NN, dURot_NN, dUAlpha_NN, YspM_NN, YspP_NN, YmMin_NN, Tempos_NN, PHI_NN), f)
 
-with open('NNMPC/libs/resultados_CAMPC.pkl', 'wb') as f:
-    pickle.dump((iter_CA, Ymk_CA, Ypk_CA, Upk_CA, dURot_CA, dUAlpha_CA, YspM_CA, YspP_CA, YmMin_CA, Tempos_CA, PHI_CA), f)
+#with open('NNMPC/libs/resultados_CAMPC.pkl', 'wb') as f:
+    #pickle.dump((iter_CA, Ymk_CA, Ypk_CA, Upk_CA, dURot_CA, dUAlpha_CA, YspM_CA, YspP_CA, YmMin_CA, Tempos_CA, PHI_CA), f)
